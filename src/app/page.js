0ee901bedd8acc07f0304dfb756eb91738fe51c9
Hancock Ethan M <hancockethanm@johndeere.com>
2023-06-14 20:07:36 -0500

    Remove eslint

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      Hey, I'm Ethan!
+        <p>Hey, I'm Ethan!</p>
     </main>
   )
 }
