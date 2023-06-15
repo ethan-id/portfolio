fb2e809172107cb1d7773d96c6c5ff6702b238c9
Hancock Ethan M <hancockethanm@johndeere.com>
2023-06-14 19:59:21 -0500

    Make <main>

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.main}>
+    <main className={styles.main}>
       Hey, I'm Ethan!
-    </div>
+    </main>
   )
 }
