6d6d9291b117273ef42b929f238f07d875259f96
Hancock Ethan M <hancockethanm@johndeere.com>
2023-06-14 21:49:49 -0500

    Add navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import styles from './page.module.css';
 import React from 'react';
 import {Analytics} from '@vercel/analytics/react';
+import NavBar from './NavBar';
 
 export default function Home() {
     return (
         <>
-            <main className={styles.main}>
+            <NavBar/>
+            <main className={styles.body}>
                 <p>Hey, I'm Ethan!</p>
             </main>
             <Analytics />
