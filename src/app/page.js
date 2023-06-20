ff5708e4be67fe95768bcfe3bb95adaecf4faf4b
Hancock Ethan M <hancockethanm@johndeere.com>
2023-06-20 18:21:42 -0500

    Refactor file structure and use tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,15 @@
-import styles from './page.module.css';
+import styles from './page.module.scss'
 import React from 'react';
 import {Analytics} from '@vercel/analytics/react';
-import NavBar from './NavBar';
+import NavBar from '@/components/NavBar/NavBar';
+import {LandingPage} from '@/components/LandingPage/LandingPage';
 
 export default function Home() {
     return (
-        <>
+        <div className={styles.app}>
             <NavBar/>
-            <main className={styles.body}>
-                <div className={styles.nameCont}>
-                    <div className={styles.software}>
-                        Software
-                        &nbsp;&nbsp;&nbsp;&nbsp;Engineer
-                    </div>
-                    <img
-                        alt="picture of garfield sitting in front of a macbook"
-                        className="rounded-full"
-                        src="https://www.github.com/ethan-id.png"
-                    />
-                </div>
-            </main>
+            <LandingPage/>
             <Analytics />
-        </>
+        </div>
     )
 }
