76536aac99754b9b0f5d363f8338b4fe1ea086a7
ethan-id <ehancock1648@gmail.com>
2023-06-29 02:57:38 -0500

    Added pic

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,21 +2,29 @@ import styles from './LandingPage.module.scss';
 import React from 'react';
 import Image from 'next/image'
 import github from './github.png'
+import me from './IMG_1317.jpg';
 import Experience from '../Experience/Experience';
 
 export const LandingPage = () => {
     return (
         <div className='flex flex-col mx-10 2xl:mx-60'>
-            <div className='my-20 2xl:ml-20 2xl:my-48 leading-normal'>
-                <div className='text-5xl lg:text-8xl font-bold'>
-                    Hey, I'm Ethan!
+            <div className='flex flex-col 2xl:my-20 2xl:flex-row justify-between'>
+                <div className='flex flex-col my-20 2xl:ml-20 2xl:my-48 leading-normal'>
+                    <div className='text-5xl lg:text-8xl font-bold'>
+                        Hey, I'm Ethan!
+                    </div>
+                    <div className='text-3xl ml-20 mt-5 lg:mt-10 lg:text-7xl font-normal'>
+                        I do a little coding sometimes
+                    </div>
                 </div>
-                <div className='text-3xl ml-20 mt-5 lg:mt-10 lg:text-7xl font-normal'>
-                    I do a little coding sometimes
-                </div>
+                <Image  
+                    src={me}
+                    className="dark:block w-full rounded-full mb-20 w-48 h-48 2xl:w-96 2xl:h-96 m-auto"
+                    alt="Screenshot of my github profile"
+                />
             </div>
 
-            <div className="flex flex-col 2xl:flex-row m-auto mb-40 2xl:mt-40">
+            <div className="flex flex-col 2xl:flex-row m-auto mb-20 2xl:mb-48 2xl:mt-40">
                 <div className="text-2xl lg:text-6xl my-auto text-center leading-tight">
                     Check out my <a className="text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-purple-600" href="https://www.github.com/ethan-id">GitHub</a> to see my most recent projects!
                 </div>
