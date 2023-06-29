467d964f3d6e34f2e82aaefa202adcdcfa9ef779
ethan-id <ehancock1648@gmail.com>
2023-06-29 14:46:09 -0500

    Add footer and fancy styles

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../NavBar/NavBar.module.scss'
+import classNames from 'classnames';
+
+const Footer = () => {
+    return (
+        <nav className={classNames("border-t-8 border-solid border-blue-300 flex flex-col 2xl:flex-row justify-between mx-auto p-10 px-14 lg:px-28", styles.footerBorder)}>
+            <span className="self-center text-3xl sm:text-4xl font-semibold whitespace-nowrap dark:text-white">
+                {'</Ethan Hancock>'}
+            </span>
+        </nav>
+    )
+}
+
+export default Footer;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import Image from 'next/image';
+import ig from './ig.png';
+import styles from './NavBar.module.scss';
+import classNames from 'classnames';
 
 const NavBar = () => {
     return (
-        <nav className="border-b-2 border-solid border-blue-300 flex flex-wrap items-center justify-between mx-auto p-10 px-14 lg:px-28">
+        <nav className={classNames("border-b-8 border-solid border-blue-300 flex flex-col 2xl:flex-row justify-between mx-auto p-10 px-14 lg:px-28", styles.navBorder)}>
             <span className="self-center text-3xl sm:text-4xl font-semibold whitespace-nowrap dark:text-white">
-                {'< Ethan Hancock />'}
+                {'<Ethan Hancock>'}
             </span>
+
+            <div className="flex flex-row justify-center gap-x-4 mt-5">
+                <a href="https://www.linkedin.com/in/ethanhancock23">
+                    <img className="h-8 lg:h-12 w-8 lg:w-12" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="LinkedIn Logo"/>
+                </a>
+                <a href="https://www.instagram.com/ethan.idfk/">
+                    <Image  
+                        src={ig}
+                        className="h-8 lg:h-12 w-8 lg:w-12"
+                        alt="Screenshot of my github profile"
+                    />
+                </a>
+            </div>
 
             {/* <button>
                 <svg className="h-12" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
