8aca66096b60c6aa358a9a6604fc03b0da3b897c
Hancock Ethan M <hancockethanm@johndeere.com>
2023-07-18 12:04:56 -0500

    Improve site metadata for SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,12 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Ethan\'s Portfolio',
-  description: 'Personal portfolio website for programming projects and my resume',
+  title: 'Ethan Hancock - Full-Stack Software Engineer Portfolio',
+  description: 'Explore the full-stack software engineering portfolio of Ethan Hancock.' +
+      ' Specializing in Java, Python, JavaScript, TypeScript, and more, Ethan Hancock has ' +
+      'a diverse range of skills and experiences. Discover personal projects including a ' +
+      'Java-based Minesweeper clone, a Next.js personal website, and a Puppeteer project ' +
+      'for stock market analysis. Check out Ethan Hancock\'s GitHub for more.',
 }
 
 export default function RootLayout({ children }) {
