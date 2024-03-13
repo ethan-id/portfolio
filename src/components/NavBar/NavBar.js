b58842f40c6ac6488323023c0a54a1b1b2b66173
ehancock <ehancock@iastate.edu>
2024-03-12 22:35:51 -0500

    Add badge to Dinder

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,7 +10,7 @@ import classNames from 'classnames';
 
 const NavBar = () => {
     return (
-        <nav>
+        <section>
             <nav className="flex flex-col 2xl:flex-row justify-between mx-auto p-10 px-14 lg:px-28">
                 <span className="self-center text-3xl sm:text-4xl font-semibold whitespace-nowrap text-white">
                     {'<Ethan Hancock>'}
@@ -35,15 +35,9 @@ const NavBar = () => {
                         />
                     </a>
                 </div>
-
-                {/* <button>
-                    <svg className="h-12" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
-                    </svg>
-                </button> */}
             </nav>
             <div className={styles.navBorder}></div>
-        </nav>
+        </section>
     )
 }
 
