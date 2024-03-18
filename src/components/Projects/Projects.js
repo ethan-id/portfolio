82a4c532587ba91ae73a788dcea9ac80364fb02c
ehancock <ehancock@iastate.edu>
2024-03-17 19:06:01 -0500

    Add GPT projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,8 @@ import Image from 'next/image';
 import BizGlimpseLogo from './Logo.png';
 import DinderLogo from './dinder_logo_512.png';
 import YahooScraperLogo from './yahoo-scraper.png';
+import OpenAI from './openai.svg';
+import AI from './ai.jpg';
 import classNames from 'classnames';
 import styles from '../LandingPage/LandingPage.module.scss';
 import { TypewriterEffect } from '../shared/TypeWriterEffect';
@@ -12,7 +14,7 @@ import { TypewriterEffect } from '../shared/TypeWriterEffect';
 export const Projects = () => {
 
     return (
-      <section className='shadow-inset mt-40 py-20 border-none bg-gradient-to-r from-blue-300 to-purple-600'> 
+      <section className='shadow-inset flex flex-col gap-8 mt-40 py-20 border-none bg-gradient-to-r from-blue-300 to-purple-600'> 
         <TypewriterEffect 
             words={[{text: 'Projects', className: 'text-5xl lg:text-6xl my-auto text-center leading-tight mb-12 font-bold text-gray-900 dark:text-gray-900'}]}
             cursorClassName={'hidden'}
@@ -38,7 +40,7 @@ export const Projects = () => {
                     </div>
                 </CardHeader>
                 <CardBody>
-                    <p>Get a glimpse of any publicly traded company via its ticker symbol.</p>
+                    <p>Get a glimpse of any publicly traded company via its ticker symbol, and chat with ChatGPT about the data (using GPT Connector)</p>
                 </CardBody>
                 <CardFooter>
                     <Link
@@ -70,7 +72,7 @@ export const Projects = () => {
                     </div>
                 </CardHeader>
                 <CardBody>
-                    <p>Get matched with local restaurants based on your preferences or your group's.</p>
+                    <p>Get matched with local restaurants based on your preferences or your group's</p>
                     <Chip className='bg-amber-300 mt-5'>👑 Best Project Award 👑</Chip>
                 </CardBody>
                 <CardFooter>
@@ -89,6 +91,58 @@ export const Projects = () => {
                     <Image
                         alt="Yahoo Finance Web Scraper Logo"
                         className='w-12 lg:w-24'
+                        src={OpenAI}
+                    />
+                    <div className="flex flex-col">
+                        <p className="text-xl lg:text-3xl">GPT Connector</p>
+                    </div>
+                </CardHeader>
+                <CardBody>
+                    <p className='overflow-hidden'>A Flask API for an AI assistant built using R.A.G. with OpenAI's GPT-3.5-turbo for data queries; Deployed on Google Cloud Run for usage in BizGlimpse</p>
+                </CardBody>
+                <CardFooter>
+                    <Link
+                        isExternal
+                        showAnchorIcon
+                        href='https://github.com/ethan-id/gpt-connector'
+                    >
+                        View source code on GitHub
+                    </Link>
+                </CardFooter>
+            </Card>
+        </div>
+
+        <div className='flex flex-col lg:flex-row justify-center items-center gap-8'>
+            <Card isBlurred className={classNames(styles.grow, "shadow-2xl max-w-[400px] border-2 border-black rounded-2xl p-4 w-5/6 h-80 text-black")}>
+                <CardHeader className="flex gap-3">
+                    <Image
+                        alt="Yahoo Finance Web Scraper Logo"
+                        className='w-12 lg:w-24 rounded-full'
+                        src={AI}
+                    />
+                    <div className="flex flex-col">
+                        <p className="text-xl lg:text-3xl">ChatGPT Clone</p>
+                    </div>
+                </CardHeader>
+                <CardBody>
+                    <p>User Interface built with NextJS and TypeScript allowing interaction with OpenAI's ChatGPT</p>
+                </CardBody>
+                <CardFooter>
+                    <Link
+                        isExternal
+                        showAnchorIcon
+                        href='https://github.com/ethan-id/chat-gpt-clone'
+                    >
+                        View source code on GitHub
+                    </Link>
+                </CardFooter>
+            </Card>
+
+            <Card isBlurred className={classNames(styles.grow, "shadow-2xl max-w-[400px] border-2 border-black rounded-2xl p-4 w-5/6 h-80 text-black")}>
+                <CardHeader className="flex gap-3">
+                    <Image
+                        alt="Yahoo Finance Web Scraper Logo"
+                        className='w-12 lg:w-24'
                         src={YahooScraperLogo}
                     />
                     <div className="flex flex-col">
@@ -96,7 +150,7 @@ export const Projects = () => {
                     </div>
                 </CardHeader>
                 <CardBody>
-                    <p>Web-scraper used to gather over 500 data points, all scraped in real-time from Yahoo Finance.</p>
+                    <p>Web-scraper used to gather over 500 data points, all scraped in real-time from Yahoo Finance</p>
                 </CardBody>
                 <CardFooter>
                     <Link
