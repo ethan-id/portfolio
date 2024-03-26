7aaba22725d9762e2566862c0d463ca89112d0c6
ehancock <ehancock@iastate.edu>
2024-03-26 18:15:51 -0500

    update open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,40 +5,49 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  metadataBase: new URL('https://www.ethanhancock.org'),
-  generator: 'Next.js',
-  title: 'Ethan Hancock - Full-Stack Software Engineer Portfolio',
-  applicationName: 'Ethan Hancock - Full-Stack Software Engineer Portfolio',
-  description: "Explore the full-stack software engineering portfolio of Ethan Hancock. Specializing in Java, Python, JavaScript, TypeScript, and more, Ethan has a diverse range of skills and experiences. Discover personal projects including a Java-based Minesweeper clone, a Next.js personal website, and a Puppeteer project for stock market analysis. Check out Ethan's GitHub for more.",
-  referrer: 'origin-when-cross-origin',
-  keywords: ['Full-Stack', 'Software Engineer', 'Java', 'Python', 'JavaScript', 'TypeScript',
-    'React', 'Redux', 'SASS', 'Tailwind', 'Bootstrap', 'Next.js', 'MongoDB', 'Node.js',
-    'Full-Stack Developer', 'Web Development', 'Front-end Development', 'Back-end Development',
-    'Full-Stack Projects', 'Software Development', 'Web Application Development', 'JavaScript Developer',
-    'Python Developer', 'Java Developer', 'React Developer', 'Node.js Developer', 'Software Engineering Portfolio',
-    'Full-Stack Software Engineer Skills', 'Software Engineer Personal Website', 'Software Engineer Resume',
-    'Software Engineer GitHub Projects'],
-  authors: [{ name: 'Ethan Hancock', url: 'https://www.ethanhancock.org' }],
-  colorScheme: 'dark',
-  creator: 'Ethan Hancock',
-  publisher: 'Ethan Hancock',
-  robots: 'index, follow',
-  openGraph: {
-    title: 'Ethan Hancock - Software Engineer',
-    description: "Explore the software engineering portfolio of Ethan Hancock. Specializing in avaScript, TypeScript, Java, Python, and more, Ethan has a diverse range of skills and experiences.",
-    url: 'https://www.ethanhancock.org',
-    type: 'website',
-  },
-  category: 'Software Engineering'
+    metadataBase: new URL('https://www.ethanhancock.org'),
+    generator: 'Next.js',
+    title: 'Ethan Hancock - Full-Stack Software Engineer Portfolio',
+    applicationName: 'Ethan Hancock - Full-Stack Software Engineer Portfolio',
+    description: "Explore the full-stack software engineering portfolio of Ethan Hancock. Specializing in Java, Python, JavaScript, TypeScript, and more, Ethan has a diverse range of skills and experiences. Discover personal projects including a Java-based Minesweeper clone, a Next.js personal website, and a Puppeteer project for stock market analysis. Check out Ethan's GitHub for more.",
+    referrer: 'origin-when-cross-origin',
+    keywords: ['Full-Stack', 'Software Engineer', 'Java', 'Python', 'JavaScript', 'TypeScript',
+        'React', 'Redux', 'SASS', 'Tailwind', 'Bootstrap', 'Next.js', 'MongoDB', 'Node.js',
+        'Full-Stack Developer', 'Web Development', 'Front-end Development', 'Back-end Development',
+        'Full-Stack Projects', 'Software Development', 'Web Application Development', 'JavaScript Developer',
+        'Python Developer', 'Java Developer', 'React Developer', 'Node.js Developer', 'Software Engineering Portfolio',
+        'Full-Stack Software Engineer Skills', 'Software Engineer Personal Website', 'Software Engineer Resume',
+        'Software Engineer GitHub Projects'],
+    authors: [{ name: 'Ethan Hancock', url: 'https://www.ethanhancock.org' }],
+    colorScheme: 'dark',
+    creator: 'Ethan Hancock',
+    publisher: 'Ethan Hancock',
+    robots: 'index, follow',
+    openGraph: {
+        title: 'Ethan Hancock - Software Engineer',
+        description: "Explore the software engineering portfolio of Ethan Hancock. Specializing in avaScript, TypeScript, Java, Python, and more, Ethan has a diverse range of skills and experiences.",
+        url: 'https://www.ethanhancock.org',
+        type: 'website',
+        siteName: 'Ethan Hancock - Software Engineer',
+        images: [
+            {
+                url: 'https://www.ethanhancock.org/me.png',
+                width: 1171,
+                height: 1171,
+                alt: 'Ethan Hancock'
+            }
+        ]
+    },
+    category: 'Software Engineering'
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <SpeedInsights/>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                {children}
+                <SpeedInsights/>
+            </body>
+        </html>
+    )
 }
