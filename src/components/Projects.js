b7fe83f1f5e9b69e2db9fa92cb63dccb7ef1f53f
ehancock <ehancock@iastate.edu>
2025-01-15 21:27:18 -0600

    Add new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,233 +1,129 @@
 'use client';
 
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Chip,
-    Link
-} from '@nextui-org/react';
+import {Card, CardHeader, CardBody, CardFooter, Chip, Link} from '@nextui-org/react';
 import Image from 'next/image';
 import BizGlimpseLogo from '@/images/Logo.png';
 import DinderLogo from '@/images/dinder_logo_512.png';
 import YahooScraperLogo from '@/images/yahoo-scraper.png';
 import OpenAI from '@/images/openai.svg';
 import AI from '@/images/ai.jpg';
-import classNames from 'classnames';
-import styles from './LandingPage.module.scss';
+import Tree from '@/images/tree.png';
+import ScrabbleSearch from '@/images/scrabble-search.png';
 import {TypewriterEffect} from './shared/TypeWriterEffect';
 
 export const Projects = () => {
+    const projects = [
+        {
+            name: 'National Parks Finder',
+            logo: Tree,
+            description: 'See all of the National Park Services designated National Parks and learn more about them!',
+            rounded: false,
+            link: 'https://national-parks-finder.vercel.app/',
+            github: 'https://github.com/ethan-id/national-parks-finder'
+        },
+        {
+            name: 'Dinder',
+            logo: DinderLogo,
+            description: "Get matched with local restaurants based on your preferences or your group's",
+            link: 'https://www.youtube.com/watch?v=DoNjafJZINw&t=1s&ab_channel=EthanHancock',
+            github: 'https://github.com/ethan-id/Dinder',
+            award: '👑 Best Project Award 👑'
+        },
+        {
+            name: 'Scrabble Search',
+            logo: ScrabbleSearch,
+            description:
+                'Search across 170k+ valid scrabble words, see their point values, and calculate scores of your own words!',
+            rounded: false,
+            link: 'https://scrabble-search.vercel.app/',
+            github: 'https://github.com/ethan-id/scrabble-search'
+        },
+        {
+            name: 'BizGlimpse',
+            logo: BizGlimpseLogo,
+            description:
+                'Get a glimpse of any publicly traded company via its ticker symbol, and chat with ChatGPT about the data (using GPT Connector)',
+            link: 'https://biz-glimpse.vercel.app',
+            github: 'https://github.com/ethan-id/BizGlimpse'
+        },
+        {
+            name: 'GPT Connector',
+            logo: OpenAI,
+            description:
+                "A Flask API for an AI assistant built using R.A.G. with OpenAI's GPT-3.5-turbo for data queries; Deployed on Google Cloud Run for usage in BizGlimpse",
+            github: 'https://github.com/ethan-id/gpt-connector'
+        },
+        {
+            name: 'Yahoo Finance Web Scraper',
+            logo: YahooScraperLogo,
+            rounded: false,
+            description: 'Web-scraper used to gather over 500 data points, all scraped in real-time from Yahoo Finance',
+            github: 'https://github.com/ProjectGreenBoat/yahoo-finance-scraper'
+        }
+    ];
+
     return (
         <section
-            className='shadow-inset flex flex-col gap-8 mt-40 py-20 border-none bg-gradient-to-r from-blue-300 to-purple-600'
+            className='bg-gradient-to-r from-blue-500 to-purple-600 py-20 mt-20'
             id='projects'
         >
-            <TypewriterEffect
-                words={[
-                    {
-                        text: 'Projects',
-                        className:
-                            'text-5xl lg:text-6xl my-auto text-center leading-tight mb-12 font-bold text-gray-900 dark:text-gray-900'
-                    }
-                ]}
-                cursorClassName={'hidden'}
-            />
-
-            <div className='flex flex-col lg:flex-row justify-center items-center gap-8'>
-                <Card
-                    isBlurred
-                    className={classNames(
-                        styles.grow,
-                        'shadow-2xl max-w-[400px] border-2 border-black rounded-2xl p-4 w-5/6 h-80 text-black'
-                    )}
-                >
-                    <CardHeader className='flex gap-3'>
-                        <Image
-                            alt='BizGlimpse Logo'
-                            className='w-12 lg:w-24 bg-white rounded-full'
-                            src={BizGlimpseLogo}
-                        />
-                        <div className='flex flex-col'>
-                            <p className='text-xl lg:text-3xl'>BizGlimpse</p>
-                            <Link
-                                isExternal
-                                showAnchorIcon
-                                href='https://biz-glimpse.vercel.app'
-                            >
-                                biz-glimpse.vercel.app
-                            </Link>
-                        </div>
-                    </CardHeader>
-                    <CardBody>
-                        <p className='overflow-hidden'>
-                            Get a glimpse of any publicly traded company via its
-                            ticker symbol, and chat with ChatGPT about the data
-                            (using GPT Connector)
-                        </p>
-                    </CardBody>
-                    <CardFooter>
-                        <Link
-                            isExternal
-                            showAnchorIcon
-                            href='https://github.com/ethan-id/BizGlimpse'
+            <div className='container mx-auto px-4'>
+                <TypewriterEffect
+                    words={[
+                        {
+                            text: 'Projects',
+                            className: 'text-5xl lg:text-6xl font-bold text-white text-center mb-12'
+                        }
+                    ]}
+                    cursorClassName={'hidden'}
+                />
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                    {projects.map((project, index) => (
+                        <Card
+                            key={index}
+                            className='bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg border border-white border-opacity-20 rounded-xl overflow-hidden transform transition duration-300 hover:scale-105'
                         >
-                            View source code on GitHub
-                        </Link>
-                    </CardFooter>
-                </Card>
-
-                <Card
-                    isBlurred
-                    className={classNames(
-                        styles.grow,
-                        'shadow-2xl max-w-[400px] border-2 border-black rounded-2xl p-4 w-5/6 h-80 text-black'
-                    )}
-                >
-                    <CardHeader className='flex gap-3'>
-                        <Image
-                            alt='Dinder Logo'
-                            className='w-12 lg:w-24 bg-black rounded-full'
-                            src={DinderLogo}
-                        />
-                        <div className='flex flex-col'>
-                            <p className='text-xl lg:text-3xl'>Dinder</p>
-                            <Link
-                                isExternal
-                                showAnchorIcon
-                                href='https://www.youtube.com/watch?v=DoNjafJZINw&t=1s&ab_channel=EthanHancock'
-                            >
-                                View demo video
-                            </Link>
-                        </div>
-                    </CardHeader>
-                    <CardBody>
-                        <p>
-                            Get matched with local restaurants based on your
-                            preferences or your group's
-                        </p>
-                        <Chip className='bg-amber-300 mt-5'>
-                            👑 Best Project Award 👑
-                        </Chip>
-                    </CardBody>
-                    <CardFooter>
-                        <Link
-                            isExternal
-                            showAnchorIcon
-                            href='https://github.com/ethan-id/Dinder'
-                        >
-                            View source code on GitHub
-                        </Link>
-                    </CardFooter>
-                </Card>
-
-                <Card
-                    isBlurred
-                    className={classNames(
-                        styles.grow,
-                        'shadow-2xl max-w-[400px] border-2 border-black rounded-2xl p-4 w-5/6 h-80 text-black'
-                    )}
-                >
-                    <CardHeader className='flex gap-3'>
-                        <Image
-                            alt='Yahoo Finance Web Scraper Logo'
-                            className='w-12 lg:w-24'
-                            src={OpenAI}
-                        />
-                        <div className='flex flex-col'>
-                            <p className='text-xl lg:text-3xl'>GPT Connector</p>
-                        </div>
-                    </CardHeader>
-                    <CardBody>
-                        <p className='overflow-hidden'>
-                            A Flask API for an AI assistant built using R.A.G.
-                            with OpenAI's GPT-3.5-turbo for data queries;
-                            Deployed on Google Cloud Run for usage in BizGlimpse
-                        </p>
-                    </CardBody>
-                    <CardFooter>
-                        <Link
-                            isExternal
-                            showAnchorIcon
-                            href='https://github.com/ethan-id/gpt-connector'
-                        >
-                            View source code on GitHub
-                        </Link>
-                    </CardFooter>
-                </Card>
-            </div>
-
-            <div className='flex flex-col lg:flex-row justify-center items-center gap-8'>
-                <Card
-                    isBlurred
-                    className={classNames(
-                        styles.grow,
-                        'shadow-2xl max-w-[400px] border-2 border-black rounded-2xl p-4 w-5/6 h-80 text-black'
-                    )}
-                >
-                    <CardHeader className='flex gap-3'>
-                        <Image
-                            alt='Yahoo Finance Web Scraper Logo'
-                            className='w-12 lg:w-24 rounded-full'
-                            src={AI}
-                        />
-                        <div className='flex flex-col'>
-                            <p className='text-xl lg:text-3xl'>ChatGPT Clone</p>
-                        </div>
-                    </CardHeader>
-                    <CardBody>
-                        <p>
-                            User Interface built with NextJS and TypeScript
-                            allowing interaction with OpenAI's ChatGPT
-                        </p>
-                    </CardBody>
-                    <CardFooter>
-                        <Link
-                            isExternal
-                            showAnchorIcon
-                            href='https://github.com/ethan-id/chat-gpt-clone'
-                        >
-                            View source code on GitHub
-                        </Link>
-                    </CardFooter>
-                </Card>
-
-                <Card
-                    isBlurred
-                    className={classNames(
-                        styles.grow,
-                        'shadow-2xl max-w-[400px] border-2 border-black rounded-2xl p-4 w-5/6 h-80 text-black'
-                    )}
-                >
-                    <CardHeader className='flex gap-3'>
-                        <Image
-                            alt='Yahoo Finance Web Scraper Logo'
-                            className='w-12 lg:w-24'
-                            src={YahooScraperLogo}
-                        />
-                        <div className='flex flex-col'>
-                            <p className='text-xl lg:text-3xl'>
-                                Yahoo Finance Web Scraper
-                            </p>
-                        </div>
-                    </CardHeader>
-                    <CardBody>
-                        <p>
-                            Web-scraper used to gather over 500 data points, all
-                            scraped in real-time from Yahoo Finance
-                        </p>
-                    </CardBody>
-                    <CardFooter>
-                        <Link
-                            isExternal
-                            showAnchorIcon
-                            href='https://github.com/ProjectGreenBoat/yahoo-finance-scraper'
-                        >
-                            View source code on GitHub
-                        </Link>
-                    </CardFooter>
-                </Card>
+                            <CardHeader className='flex items-center gap-4 p-6'>
+                                <Image
+                                    alt={`${project.name} Logo`}
+                                    className={` w-16 h-16 ${project.rounded === false ? '' : 'rounded-full'}`}
+                                    src={project.logo || '/placeholder.svg'}
+                                />
+                                <div>
+                                    <h3 className='text-2xl font-semibold text-white'>{project.name}</h3>
+                                    {project.link && (
+                                        <Link
+                                            isExternal
+                                            showAnchorIcon
+                                            href={project.link}
+                                            className='text-blue-200 hover:text-blue-100'
+                                        >
+                                            View Project
+                                        </Link>
+                                    )}
+                                </div>
+                            </CardHeader>
+                            <CardBody className='p-6'>
+                                <p className='text-white text-opacity-80'>{project.description}</p>
+                                {project.award && (
+                                    <Chip className='bg-yellow-400 text-gray-900 mt-4 font-semibold'>
+                                        {project.award}
+                                    </Chip>
+                                )}
+                            </CardBody>
+                            <CardFooter className='p-6'>
+                                <Link
+                                    isExternal
+                                    showAnchorIcon
+                                    href={project.github}
+                                    className='text-blue-200 hover:text-blue-100'
+                                >
+                                    View source code on GitHub
+                                </Link>
+                            </CardFooter>
+                        </Card>
+                    ))}
+                </div>
             </div>
         </section>
     );
